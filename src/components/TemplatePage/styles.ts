f5ab2e0a4fb3399b3199ae874337becba47090fd
melinmiguel <melinmiguel@gmail.com>
2021-05-06 19:00:34 -0300

    adicionando efeito no botao

--- src/components/TemplatePage/styles.ts
+++ src/components/TemplatePage/styles.ts
@@ -71,17 +71,39 @@ export const InputSenha = styled.input`
 `;
 
 export const BotaoEntrar = styled.button`
-  background: #2d62ed 0% 0% no-repeat padding-box;
-  border-radius: 4px;
-  opacity: 1;
-  width: 672px;
-  height: 91px;
-
-  text-align: center;
-  font: normal normal bold 26px/39px Poppins;
-  letter-spacing: 0px;
+  position: relative;
+  background-color: #2d62ed;
+  border: none;
+  font-size: 28px;
   color: #ffffff;
-  opacity: 1;
+  padding: 20px;
+  width: 671px;
+  text-align: center;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  text-decoration: none;
+  overflow: hidden;
+  cursor: pointer;
+
+  :after {
+    content: '';
+    background: #ffffff;
+    display: block;
+    position: absolute;
+    padding-top: 300%;
+    padding-left: 300%;
+    margin-left: -20px !important;
+    margin-top: -120%;
+    opacity: 0;
+    transition: all 0.8s;
+  }
+
+  :active:after {
+    padding: 0;
+    margin: 0;
+    opacity: 1;
+    transition: 0s;
+  }
 `;
 
 export const TextContainer = styled.div`
